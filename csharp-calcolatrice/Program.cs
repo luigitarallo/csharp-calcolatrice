70367082a32988c9c7e88fc246a65276d32c9805
luigitarallo <tarallo.gigi@gmail.com>
2024-04-23 16:24:22 +0200

    add examples

--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -5,40 +5,55 @@
         static void Main(string[] args)
         {
             // Somma di due numeri interi
-            CalcoliHelper.Somma(5, 3);
+            
+            int sommaInteri = CalcoliHelper.Somma(5, 3);
+            Console.WriteLine($"La somma tra 3 e 5 è {sommaInteri}");
 
             // Somma di due numeri double
-            CalcoliHelper.Somma(5.5, 3.2);
+            double sommaDouble = CalcoliHelper.Somma(5.5, 3.2);
+            Console.WriteLine($"La somma tra 5,5 e 3,2 è {sommaDouble}");
 
             // Differenza tra due numeri interi
-            CalcoliHelper.Differenza(10, 3);
+            int differenzaInt = CalcoliHelper.Differenza(10, 3);
+            Console.WriteLine($"La differenza tra 10 e 3 è {differenzaInt}");
 
             // Differenza tra due numeri double
-            CalcoliHelper.Differenza(10.5, 3.8);
+            double differenzaDouble = CalcoliHelper.Differenza(10.5, 3.8);
+            Console.WriteLine($"La differenza tra 10,5 e 3,8 è {differenzaDouble}");
 
             // Moltiplicazione di due numeri interi
-            CalcoliHelper.Moltiplicazione(5, 3);
+            int moltiplicazioneInt = CalcoliHelper.Moltiplicazione(5, 3);
+            Console.WriteLine($"La moltiplicazione tra 5 e 3 è {moltiplicazioneInt}");
 
             // Moltiplicazione di due numeri double
-            CalcoliHelper.Moltiplicazione(5.5, 3.5);
+            double moltiplicazioneDouble = CalcoliHelper.Moltiplicazione(5.5, 3.5);
+            Console.WriteLine($"La moltiplicazione tra 5,5 e 3,5 è {moltiplicazioneDouble}");
 
             // Valore assoluto di un numero intero
-            CalcoliHelper.ValoreAssoluto(-5);
+            int valoreAssolutoInt = CalcoliHelper.ValoreAssoluto(-5);
+            Console.WriteLine($"Il valore assoluto di -5 è {valoreAssolutoInt}");
 
             // Valore assoluto di un numero double
-            CalcoliHelper.ValoreAssoluto(-5.5);
+            double valoreAssolutoDouble = CalcoliHelper.ValoreAssoluto(-5.5);
+            Console.WriteLine($"Il valore assoluto di -5,5 è {valoreAssolutoDouble}");
 
             // Minimo tra due numeri interi
-            CalcoliHelper.Minimo(5, 3);
+            int minimoInt = CalcoliHelper.Minimo(5, 3);
+            Console.WriteLine($"Il minimo tra 5 e 3 è {minimoInt}");
 
             // Minimo tra due numeri double
-            CalcoliHelper.Minimo(5.5, 3.5);
+            double minimoDouble = CalcoliHelper.Minimo(5.5, 3.5);
+            Console.WriteLine($"Il minimo tra 5,5 e 3,5 è {minimoDouble}");
 
             // Massimo tra due numeri interi
-            CalcoliHelper.Massimo(5, 3);
+            int massimoInt = CalcoliHelper.Massimo(5, 3);
+            Console.WriteLine($"Il massimo tra 5 e 3 è {massimoInt}");
 
             // Massimo tra due numeri double
-            CalcoliHelper.Massimo(5.5, 3.5);
+            double massimoDouble = CalcoliHelper.Massimo(5.5, 3.5);
+            Console.WriteLine($"Il massimo tra 5,5 e 3,5 è {massimoDouble}");
+
+
 
         }
     }
