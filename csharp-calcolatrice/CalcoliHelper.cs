debce44b83f2f413a727ac3d4501b5d16fa4b673
luigitarallo <tarallo.gigi@gmail.com>
2024-04-23 14:11:53 +0200

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal static class CalcoliHelper
+    {
+        // Somma di due numeri interi
+
+        // Somma di due numeri double
+
+        // Differenza tra due numeri interi
+
+        // Differenza tra due numeri double
+
+        // Moltiplicazione di due numeri interi
+
+        // Moltiplicazione di due numeri double
+
+        // Moltiplicazione di due numeri double
+
+        // Valore assoluto di un numero double
+
+        // Minimo tra due numeri interi
+
+        // Minimo tra due numeri double
+
+        // Massimo tra due numeri interi
+
+        // Massimo tra due numeri double
+    }
+}
